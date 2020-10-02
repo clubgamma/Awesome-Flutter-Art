5918be985060685326d668e84018c51ad99dfec5
Madhav Tripathi <madhav.37.tripathi@gmail.com>
2020-10-02 13:43:23 +0530

    added covid tracker info

---
+++ madhavtripathi05/main.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  String profileUrl = 'https://github.com/madhavtripathi05';
+  var projectLinks = [
+    {'COVID_19_TRACKER', '$profileUrl/COVID_19_TRACKER'},
+  ];
+
+  projectLinks.forEach(print);
+}
