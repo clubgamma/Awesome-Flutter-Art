c407fb3afc153721d7c1f2e2c5a09975aebd2b87
Lypsa Shah <50795750+lypsa028@users.noreply.github.com>
2020-10-06 14:52:31 +0530

    lypsa028 (#19)
    * bmi calc
    
    * Create README.md
    
    * Update README.md
    
    * bmi calc readme
    
    * readme
    
    * Add files via upload
    
    * readme
    
    * readme
    
    * readme
    
    * Delete README.md
    
    * Story App made with Flutter
    
    * Add files via upload
    
    * updated readme
    
    * Update README.md
    
    Co-authored-by: LypsaShah <50795750+LypsaShah@users.noreply.github.com>
    Co-authored-by: Yash Paneliya <yashsp1158@gmail.com>

---
+++ lypsa028/story_brain.dart
@@ -0,0 +1,74 @@
+import 'story.dart';
+
+class StoryBrain {
+  int _storyNumber = 0;
+  List<Story> _storyData = [
+    Story(
+        storyTitle:
+            'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
+        choice1: 'I\'ll hop in. Thanks for the help!',
+        choice2: 'Better ask him if he\'s a murderer first.'),
+    Story(
+        storyTitle: 'He nods slowly, unphased by the question.',
+        choice1: 'At least he\'s honest. I\'ll climb in.',
+        choice2: 'Wait, I know how to change a tire.'),
+    Story(
+        storyTitle:
+            'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
+        choice1: 'I love Elton John! Hand him the cassette tape.',
+        choice2: 'It\'s him or me! You take the knife and stab him.'),
+    Story(
+        storyTitle:
+            'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
+        choice1: 'Restart',
+        choice2: ''),
+    Story(
+        storyTitle:
+            'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
+        choice1: 'Restart',
+        choice2: ''),
+    Story(
+        storyTitle:
+            'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
+        choice1: 'Restart',
+        choice2: '')
+  ];
+  String getStory() {
+    return _storyData[_storyNumber].storyTitle;
+  }
+
+  String getChoice1() {
+    return _storyData[_storyNumber].choice1;
+  }
+
+  String getChoice2() {
+    return _storyData[_storyNumber].choice2;
+  }
+
+  void nextStory(int userChoice) {
+    if (userChoice == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (userChoice == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (userChoice == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (userChoice == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (userChoice == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (userChoice == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      restart();
+    }
+  }
+
+  void restart() => _storyNumber = 0;
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
